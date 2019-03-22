ad74a88d29f408f1826dc03e9e7b6faea6ced4ec
oxholm <oxholm@adobe.com>
2019-03-22 11:56:55 -0700

    added converters for Point and Point3

--- ndarray_converter.h
+++ ndarray_converter.h
@@ -34,11 +34,6 @@ struct type_caster<cv::Mat> {
   }
 };
 
-/*
-template <typename K>
-bool setItem(const PyObject *item, K &k);
-*/
-
 template <typename T, int N>
 struct type_caster<cv::Vec<T, N>> {
  public:
@@ -53,6 +48,7 @@ struct type_caster<cv::Vec<T, N>> {
   // Convert from python to c++
   bool load(handle src, bool) {
     auto tuple = pybind11::reinterpret_borrow<pybind11::tuple>(src);
+    if (tuple.size() != N) return false;
     for (auto index = 0; index < N; index++) {
       value[index] = tuple[index].cast<T>();
     }
@@ -65,6 +61,66 @@ struct type_caster<cv::Vec<T, N>> {
     for (auto index = 0; index < N; index++) {
       mytuple[index] = T(src[index]);
     }
+    return mytuple.release();
+  }
+};
+
+template <typename T>
+struct type_caster<cv::Point_<T>> {
+ public:
+  // Alias the type
+  using Point = cv::Point_<T>;
+
+  /**
+   * This macro declares a local variable 'value' of type cv::Vec3b
+   */
+  PYBIND11_TYPE_CASTER(Point, _("tuple"));
+
+  // Convert from python to c++
+  bool load(handle src, bool) {
+    auto tuple = pybind11::reinterpret_borrow<pybind11::tuple>(src);
+    // if (tuple.size() != 2) return false;
+    value.x = tuple[0].cast<T>();
+    value.y = tuple[1].cast<T>();
+    return true;
+  }
+
+  // Convert Vec into a tuple
+  static handle cast(const Point &src, return_value_policy /* policy */, handle /* parent */) {
+    pybind11::tuple mytuple(2);
+    mytuple[0] = T(src.x);
+    mytuple[1] = T(src.y);
+    return mytuple.release();
+  }
+};
+
+template <typename T>
+struct type_caster<cv::Point3_<T>> {
+ public:
+  // Alias the type
+  using Point = cv::Point3_<T>;
+
+  /**
+   * This macro declares a local variable 'value' of type cv::Vec3b
+   */
+  PYBIND11_TYPE_CASTER(Point, _("tuple"));
+
+  // Convert from python to c++
+  bool load(handle src, bool) {
+    auto tuple = pybind11::reinterpret_borrow<pybind11::tuple>(src);
+    if (tuple.size() != 3) return false;
+    value.x = tuple[0].cast<T>();
+    value.y = tuple[1].cast<T>();
+    value.z = tuple[2].cast<T>();
+    return true;
+  }
+
+  // Convert Vec into a tuple
+  static handle cast(const Point &src, return_value_policy /* policy */, handle /* parent */) {
+    pybind11::tuple mytuple(3);
+    mytuple[0] = T(src.x);
+    mytuple[1] = T(src.y);
+    mytuple[2] = T(src.z);
     return mytuple.release();
   }
 };
